9aaeb0c5eeb6c59ff0d77793192dce09077801b1
wilberforce001 <kenmisik@gmail.com>
2023-01-06 11:47:29 +0300

    Conditional (ternary) operator

--- conditionals/index.js
+++ conditionals/index.js
@@ -1,4 +1,14 @@
+//Conditional (ternary) operator
 
+function getFee(isMember) {
+    return (isMember ? '$2.00' : '$10.00');
+}
+
+console.log(getFee(true));
+
+console.log(getFee(false));
+
+console.log(getFee(null));
 
 
 
