ea6165b2d69b2b6bc636b84b1c22060c612e17f5
wilberforce001 <kenmisik@gmail.com>
2023-02-10 11:45:29 +0300

    A program for Fizz buzz,a group word game for children to teach them about division

---
+++ problem-solving/index.js
@@ -0,0 +1,9 @@
+let answer = parseInt(prompt("Please enter the number you would like to FizzBuzz up to: "));
+
+for (let i = 1; i <= answer; i++) {
+  if (i % 3 === 0) {
+    console.log("Fizz");
+  } else {
+    console.log(i);
+  }
+}
